f45ffa99628cde3052eb8717411e9b093b45f576
Mrr Hak <longkimhak.kh@gmail.com>
2024-02-13 01:00:20 +0700

    first commit

---
+++ Controllers/AppController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_app.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AppController(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpGet]
+        public IActionResult GetAppColor()
+        {
+            string appColor = _configuration.GetValue<string>("AppSetting:AppColor") ?? "";
+            return Ok(new { appColor });
+        }
+    }
+}
