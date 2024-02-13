4df89d01facd1827c6f0815c5ad8daf53a94200e
Kimhak Long <Kimhak.Long@jtrustroyal.com>
2024-02-13 17:49:10 +0700

    update to use solution separate from project

---
+++ unit-test/UnitTestApp.cs
@@ -0,0 +1,14 @@
+namespace unit_test
+{
+    [TestClass]
+    public class UnitTestApp
+    {
+        [TestMethod]
+        public void TestGetAppColor()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
